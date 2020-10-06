b2f2ce2fe476488b7937e01e075722c5a6a80465
IAbrahamI <45631927+IAbrahamI@users.noreply.github.com>
2020-10-06 23:13:57 +0200

    added new Methode (scannForCollision) which scanns for a dot colliding into the line and stroke an oval

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -18,21 +18,21 @@ public class Controller {
 
     @FXML
     private Canvas canvas;
-    private double x1;
-    private double y1;
-    private double x2;
-    private double y2;
+
+    private double x1,y1,x2,y2;
     private Random rd = new Random();
+    private GraphicsContext context;
     private ArrayList<Dot> dots= new ArrayList<Dot>();
+
+    //---------------------------------------------------------------------
     @FXML
     void startAction(ActionEvent event) {
-
-        this.strokeSweepLine();
         this.strokeDots();
+        this.strokeSweepLine();
     }
-
+    //---------------------------------------------------------------------
     public void strokeSweepLine(){
-        GraphicsContext context = canvas.getGraphicsContext2D();
+        context = canvas.getGraphicsContext2D();
         x1 = 0;
         y1 = 0;
         x2 = 450;
@@ -42,11 +42,12 @@ public class Controller {
             public void handle(long nanotime) {
                     y1++;
                     y2++;
-                    context.clearRect(x1,y1-1,x2,y2-1);
+                    for(Dot d : dots){
+                       scannForCollision(d.getX(),d.getY());
+                    }
+                    context.clearRect(x1,y1-1,x2,2);
                     context.strokeLine(x1,y1,x2,y2);
                     context.setStroke(Color.RED);
-
-
             }
         };
         timer.start();
@@ -54,31 +55,40 @@ public class Controller {
             timer.stop();
         }
     }
-
+    //---------------------------------------------------------------------
     public void addDot(int x, int y){
         dots.add(new Dot(x,y));
     }
-
+    //---------------------------------------------------------------------
     public void createRandomDots(){
-        for(int i=1;i<=8;i++){
+        for(int i=1;i<=10;i++){
             int randomXValue = rd.nextInt(440);
             int randomYValue = rd.nextInt(590);
             this.addDot(randomXValue,randomYValue);
         }
-        for(Dot d : dots){
-            System.out.println("X: "+d.getX()+" Y: "+d.getY());
-        }
+//        for(Dot d : dots){
+//            System.out.println("X: "+d.getX()+" Y: "+d.getY());
+//        }
     }
-
+    //---------------------------------------------------------------------
     public void strokeDots(){
         this.createRandomDots();
-        GraphicsContext context = canvas.getGraphicsContext2D();
+        context = canvas.getGraphicsContext2D();
         for(Dot d: dots){
             context.setStroke(Color.GREEN);
             context.setFill(Color.GREEN);
             context.strokeOval(d.getX(),d.getY(),5,5);
             context.fillOval(d.getX(),d.getY(),5,5);
-
         }
     }
+    //---------------------------------------------------------------------
+    public void scannForCollision(int x, int y){
+        if(this.y1 == y && this.y2 == y){
+            System.out.println("Line collided with dot");
+            context = canvas.getGraphicsContext2D();
+            context.strokeOval(x-15,y-15,30,30);
+        }else{
+        }
+    }
+    //---------------------------------------------------------------------
 }
